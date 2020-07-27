4184f362d619c73447a73cc38ee274c51a8dc31b
wml <1538779315@qq.com>
2020-07-27 10:49:10 +0800

    xg

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 '''
 使用单例模式创建redis 链接池
 '''
+import requests
+from setting import proxy_pool
+headers_yxlmgw = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    }
 
-from datetime import datetime, timedelta
-
-
+url1 = 'https://www.shangniu.cn/live/lol/'
+url2 = 'http://icanhazip.com'
+response = requests.get(url2, headers_yxlmgw, proxies=proxy_pool)
+print(response.text)
+print(response)
+response1 = requests.get(url1, headers_yxlmgw, proxies=proxy_pool)
+print(response1)
--- test2.py
+++ test2.py
@@ -1,17 +1,3 @@
-# '''
-# 使用单例模式创建redis 链接池
-# '''
-import requests
-import json
-url_wzrygw = 'https://itea-cdn.qq.com/file/ingame/smoba/allMatchpage1.json'
-headers_wzrygw = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
-                         '537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
+with open('1.txt','a') as f:
+	f.write('123')
 
-
-
-requests.packages.urllib3.disable_warnings()
-response = requests.get(url=url_wzrygw, headers=headers_wzrygw, verify = False)
-response = response.text
-response = json.loads(response)
-sources = response['matchList']
-print('抓取到的源数据：',len(sources), sources)
