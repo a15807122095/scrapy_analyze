1a027d57d9f820ff63a730c0e51ec42b61481006
yghl-spider <yghl@163.com>
2020-08-11 13:44:10 +0800

    xg

--- django_spider/api_spider/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiSpiderConfig(AppConfig):
-    name = 'api_spider'
--- django_spider/api_spider/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^spider_api$', views.spider_api, name='spider_api')
-]
